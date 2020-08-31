9759910d8fa145de57ced3601c61dd96a24a92e2
cvoiculescu <corneliu@voiculescu.org>
2020-08-31 11:28:03 +0300

    Use StringUtils and added dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,23 +2,33 @@ package clinic.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords==null)?0:separateWords.length;
+	}
+	
 	private void greet(){
-		System.out.println("Hello");
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		greetings.stream().forEach((greet)->{
+			System.out.println("Greeting "+greet);
+		});
 	}
 	
     public Application() {
         System.out.println ("Inside Application");
-		List<String> list = new ArrayList<>();
-		list.stream().forEach(System.out::println);
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
-		
+		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Word count: "+count);
     }
 }
